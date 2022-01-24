c430a0142f249e57058a5fc754c5dfcc3b69357e
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2022-01-24 10:49:47 -0800

    added messages screen and started to code a flat list

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
   return (
     // <ListingDetailsScreen />
     // <WelcomeScreen />
-    <ViewImageScreen />
+    // <ViewImageScreen />
+    <MessagesScreen />
   );
 }
