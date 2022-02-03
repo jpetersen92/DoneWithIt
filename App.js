0c4b6bbf190bd69cde49d3ef111a499d612f44ce
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2022-02-03 10:38:04 -0800

    added offline notification bar and started to implement authentication for login details

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-
+import AuthContext from './app/auth/context';
 import AppNavigator from './app/navigation/AppNavigator';
+import AuthNavigator from './app/navigation/AuthNavigator'
 import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
 
 
 export default function App() {
-
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem('person', JSON.stringify({ id: 1 }))
-      const value = await AsyncStorage.getItem('person');
-      const person = JSON.parse(value)
-      console.log(person)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  demo();
-  
-
+  const [user, setUser] = useState()
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import  Constants  from 'expo-constants';
+import { useNetInfo } from '@react-native-community/netinfo';
+
+import colors from '../config/colors';
+import AppText from './AppText';
+
+function OfflineNotice(props) {
+    const netInfo = useNetInfo();
+
+    if (netInfo.type !== 'unknown' && netInfo.isInternetReachable === false)
+    return (
+        <View style={styles.container}>
+                <AppText style={styles.text}>No Internet Connection</AppText>
+        </View>
+    );
+
+    return null
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        position: 'absolute',
+        zIndex: 1,
+        top: Constants.statusBarHeight,
+        width: '100%',
+    },
+    text: {
+        color: colors.white
+    }
+})
+
+export default OfflineNotice;
