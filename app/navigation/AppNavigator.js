6502ecc644ebc33476e103ec1e75265402b7ece1
Josh Petersen <jdpetersen92@gmail.com>
2022-02-07 13:49:02 -0800

    started to add notification support

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import * as Notifications from 'expo-notifications';
+// import { Notifications } from 'expo';
+// import * as Permissions from 'expo-permissions'
 
-
+import expoPushTokensApi from '../api/expoPushTokens';
 import ListingsEditScreen from '../screens/ListingEditScreen';
 import FeedNavigator from './FeedNavigator';
 import AccountNavigator from './AccountNavigator';
@@ -11,7 +14,31 @@ import routes from './routes';
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
+const AppNavigator = () => {
+    
+    useEffect(() => {
+        registerForPushNotifications()
+
+        Notifications.addNotificationResponseReceivedListener(notification => console.log(notification))
+    }, [])
+
+    const registerForPushNotifications = async () => {
+        try {
+            // const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+            // if (!permission.granted) return;
+
+            const permission = await Notifications.requestPermissionsAsync();
+            if (!permission.granted) return;
+    
+            const token = (await Notifications.getExpoPushTokenAsync()).data;
+            console.log(token)
+            expoPushTokensApi.register(token)
+        } catch (error) {
+            console.log('Error getting a push token', error)
+        }
+    }
+
+    return (
     <Tab.Navigator screenOptions={{ headerShown: false}}>
         <Tab.Screen 
         name='Feed' 
@@ -36,6 +63,6 @@ const AppNavigator = () => (
                 <MaterialCommunityIcons name='account' color={color} size={size}/>
             }}/>
     </Tab.Navigator>
-)
+)}
 
 export default AppNavigator
